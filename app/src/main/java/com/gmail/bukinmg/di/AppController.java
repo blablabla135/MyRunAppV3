61d0627c3478f34769a238c6e74c36996abf965b
blablabla135 <bukinmg@gmail.com>
2020-02-18 16:11:20 +0200

    cxxcxxc

--- app/src/main/java/com/gmail/bukinmg/di/AppController.java
+++ app/src/main/java/com/gmail/bukinmg/di/AppController.java
@@ -3,20 +3,23 @@ package com.gmail.bukinmg.di;
 import android.app.Activity;
 import android.app.Application;
 
+
 import javax.inject.Inject;
 
 import dagger.android.DispatchingAndroidInjector;
 import dagger.android.HasActivityInjector;
+import dagger.android.HasAndroidInjector;
 
-public class AppController extends Application implements HasActivityInjector {
+public class AppController extends Application implements HasAndroidInjector {
 
     @Inject
-    DispatchingAndroidInjector<Activity> dispatchingAndroidInjector;
+    DispatchingAndroidInjector<Activity> activityInjector;
 
     @Override
     public DispatchingAndroidInjector<Activity> activityInjector() {
-        return dispatchingAndroidInjector;
+        return activityInjector;
     }
+
 
     @Override
     public void onCreate() {
